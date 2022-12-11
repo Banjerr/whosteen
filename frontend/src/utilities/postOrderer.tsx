7a862526e6dacf6f211bab51321c725e547bc90c
Benjamin Redden <benjaminredden@BenjaminsLaptop.attlocal.net>
2022-12-11 13:04:54 -0500

    more date debugging

--- frontend/src/utilities/postOrderer.tsx
+++ frontend/src/utilities/postOrderer.tsx
@@ -2,6 +2,8 @@ export default function postOrderer(data: any) {
     return data.sort((a: any, b: any) => {
         const dateA = new Date(a.date).getTime();
         const dateB = new Date(b.date).getTime();
+        console.log('a',dateA);
+        console.log('b',dateB)
         if (dateA < dateB) {
             return -1;
         }
